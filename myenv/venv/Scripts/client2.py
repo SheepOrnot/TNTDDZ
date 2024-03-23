f1160ab76caab6eeb346ca97d9e2618ccc6c3540
cty <1150166952@qq.com>
2024-03-23 22:13:02 +0800

    将验证码使用全局变量的部分更改为使用redis进行存储

--- myenv/venv/Scripts/client2.py
+++ myenv/venv/Scripts/client2.py
@@ -18,24 +18,21 @@ def leave_room():
 # 连接到服务器
 sio.connect('http://127.0.0.1:22222')
 
-
-
-#发送JSON数据到服务器
-# print("输入1创建房间，输入2加入房间")
-# select = input()
-# print("select",select)
-
+print("创建房间")
+json_data_create = '{\"account\":\"00000000000\"}'
+sio.emit('create_room', json_data_create)
 
 # elif select  == 2:
 print("加入房间")
-json_data_join = '{\"account\":\"00000000002\",\"roomid\":\"1\"}'
+json_data_join = '{\"account\":\"00000000001\",\"roomid\":\"1\"}'
 sio.emit('join_room', json_data_join)    
 
 # print("离开房间")
-# json_data_leave = '{\"account\":\"00000000002\",\"roomid\":\"1\"}'
+# json_data_leave = '{\"account\":\"00000000001\",\"roomid\":\"1\"}'
 # sio.emit('leave_room',json_data_leave)
 
-json_data_ready = '{\"account\":\"00000000002\"}'
+json_data_ready = '{\"account\":\"00000000001\"}'
 sio.emit('ready',json_data_ready)
+
+
 sio.wait()
-# 等待连接关闭
--- server.py
+++ server.py
@@ -6,19 +6,19 @@ import sqlite3 as sl
 import hashlib
 from werkzeug.utils import redirect
 import mail
-#import sendcard
+import sendcard
 from flask_socketio import SocketIO, emit, disconnect, join_room, leave_room
 from flask import Blueprint
 from flask import render_template
 import json
 import redis
-
-room_db = redis.Redis(host='127.0.0.1', port=6379, db=0)
+import random
+import threading
+redis_db = redis.Redis(host='127.0.0.1', port=6379, db=0)
 db = 'room_dbase'
 
-connected_client = []
 
-rooms = []
+
 room_id = 1
 app = Flask(__name__)
 socketio = SocketIO()
@@ -40,14 +40,18 @@ def Create_room(data):
     data = json.loads(data)
     
     data_account = data.get('account')
-    room_db.set(data_account,data_room_id)
-
+    redis_db.set(data_account,data_room_id)
+    
     join_room(room_id)
-    keys = room_db.keys()
+    check_room = str(room_id)+"_room"
+    redis_db.set(check_room,'1')
+
+
+    keys = redis_db.keys()
 
     # 遍历每个键，并输出键和对应的值
     for key in keys:
-        value = room_db.get(key)
+        value = redis_db.get(key)
         print(key.decode(), "->", value.decode())
 
     emit('server_response',room_id)
@@ -59,11 +63,11 @@ def Create_room(data):
 def Join_room(data):
     ##################################
     # 获取所有键
-    keys = room_db.keys()
+    keys = redis_db.keys()
     print("--------------------加入时候数据库内数据")
     # 遍历每个键，并输出键和对应的值
     for key in keys:
-        value = room_db.get(key)
+        value = redis_db.get(key)
         print(key.decode(), "->", value.decode())
     ##################################
 
@@ -74,7 +78,7 @@ def Join_room(data):
     count = 0
     Find = False
     for key in keys:
-        value1 = room_db.get(key)
+        value1 = redis_db.get(key)
         if value1.decode() == data_room_id:
             count += 1
             Find = True
@@ -84,6 +88,7 @@ def Join_room(data):
     
     if count >= 3:
         print("当前房间已满")
+        emit('sever_response','房间人数已满，无法加入')
         return False
     elif Find == False:
         print("加入的房间不存在")
@@ -91,10 +96,10 @@ def Join_room(data):
         
     else:
         join_room(data_room_id)
-        room_db.set(data_account,data_room_id)
+        redis_db.set(data_account,data_room_id)
         print(request.sid,"已经成功加入")
         emit('server_response','加入房间成功')
-        print("--------------------",room_db.get(data_account))
+        print("--------------------",redis_db.get(data_account))
         return True
     
     
@@ -105,11 +110,11 @@ def Leave_room(data):
 
     ##############################################
     # 获取所有键
-    keys = room_db.keys()
+    keys = redis_db.keys()
     print("--------------------删除前数据库内数据")
     # 遍历每个键，并输出键和对应的值
     for key in keys:
-        value = room_db.get(key)
+        value = redis_db.get(key)
         print(key.decode(), "->", value.decode())
     ##############################################
 
@@ -120,22 +125,70 @@ def Leave_room(data):
     print("要断开连接的数据是",data)
     data_account = data.get("account")
     data_room_id = data.get("roomid")
-    room_db.delete(data_account)
+    redis_db.delete(data_account)
     leave_room(data_room_id)
     print("删除成功")
     emit('server_response','退出房间成功')
     ######################################
     # 获取所有键
-    keys = room_db.keys()
+    keys = redis_db.keys()
     print("删除后数据库内容")
     # 遍历每个键，并输出键和对应的值
     for key in keys:
-        value = room_db.get(key)
+        value = redis_db.get(key)
         print(key.decode(), "->", value.decode())
     ######################################
     return True
+class ReadyPlayer(threading.local):
+    def __init__(self):
+        self.num = 0
+        self.account = []
+ready_player = ReadyPlayer()
+ready_player.num = 0
+ready_player.account = []
+
+@socketio.on('ready')
+def ready(data):
+    data = json.loads(data)
+    data_account = data.get("account")
+    
+    ready_player.account.append(data_account)
+    ready_player.num = ready_player.num + 1
+    room_now = room_id -1
+    print("------------------room_id",room_now)
+    emit('server_response',data_account,room = room_now)
+    print(data_account + "已经准备")
+    if ready_player.num == 3:
+        emit('server_response','所有玩家都已经准备',room = room_now)
+
+    return data_account + "已经准备" 
 
 
+class HandCards(threading.local):
+    def __init__(self):
+        all_cards = list(range(1,55))
+# 生成1到54的所有数字  
+all_numbers = list(range(1, 55))  
+  
+# 打乱数字顺序  
+random.shuffle(all_numbers)  
+  
+# 分三次取出十七个数字  
+group1 = all_numbers[:17]  
+all_numbers = all_numbers[17:]  # 移除已经取出的数字  
+  
+group2 = all_numbers[:17]  
+all_numbers = all_numbers[17:]  # 移除已经取出的数字  
+
+group3 = all_numbers[:17]  
+all_numbers = all_numbers[17:]  # 移除已经取出的数字  
+# 剩余的三个数字作为第四组  
+group4 = all_numbers  
+
+# @socketio.on('sendcard')
+# def sendCard(data):
+#     global group1,group2,
+#     sendcard.transfercard()
 
 
 @app.route('/battle',methods = ["POST"])#存储对战数据
@@ -155,11 +208,17 @@ def SignupMailPost():
     print('mail=======================',mailData)
     code = mail.generate_verification_code()
 
-    global code5minute
-    code5minute = code
+    redis_db.setex(str(mailData)+"_signupmail",300,code)
+    # 获取所有键
+    keys = redis_db.keys()
+
+    # 遍历每个键，并输出键和对应的值
+    for key in keys:
+        value = redis_db.get(key)
+        print(key.decode(), "->", value.decode())
 
     mail.send_email('18607706274@163.com','JNCMBLONBOPNOFTW',mail.write_email('18607706274@163.com',mailData,code),mailData)
-    return str(code5minute)
+    return str(code)
 
 @app.route('/signup',methods = ["POST"])#存储注册信息
 def SignupPost():
@@ -177,8 +236,9 @@ def SignupPost():
     usernameData = SignupData.get("username")
     codeData = SignupData.get("code")
     passwordData = SignupData.get("password")
-    global code5minute
-    if str(codeData) == code5minute:
+    check_code = redis_db.get(str(mailData)+"_signupmail")
+    print("redis_db.get(str(mailData)+_signupmail",check_code.decode())
+    if str(codeData) == check_code.decode():
         passwordData = hashlib.md5(passwordData.encode()).digest().hex()
         UserDataDB = [usernameData,accountData,mailData,passwordData]
         print(UserDataDB)
@@ -225,20 +285,21 @@ def LoginPost():
 @app.route('/passwordforget',methods = ["POST"])#找回密码（验证是否发生过注册）
 def PasswordForgetPost():
     PasswordForgetData = request.get_json()
-    print(PasswordForgetData)
     Findingmail = PasswordForgetData.get("mail")
     Findingcode = PasswordForgetData.get("code")
-
+    
+    print(redis_db.get(str(Findingmail)+'_passwordforgetmail'))
+    print(str(Findingcode))
     FindingNewpassword = PasswordForgetData.get("password")
     print("code",Findingcode)
     FindingLanguage = '''SELECT Umail FROM UserTable WHERE Umail like ?'''
     cursor.execute(FindingLanguage,[Findingmail])
     queryresult = cursor.fetchall()
     print(queryresult)
-    global code5minute
-    print("code5minute",code5minute)
 
-    if len(queryresult) != 0 and Findingcode == code5minute :
+    check_code =redis_db.get(str(Findingmail)+'_passwordforgetmail')
+    print(" str(Findingcode):", str(Findingcode),"check_code:",check_code.decode())
+    if len(queryresult) != 0 and str(Findingcode) == check_code.decode():
         Findingresult = True
     else:
         Findingresult = False
@@ -266,11 +327,20 @@ def PasswordForgetMailPost():
     cursor.execute(FindingLanguage,[Findingmail])
 
     code = mail.generate_verification_code()
-    global code5minute
-    code5minute = code
-    mail.send_email('18607706274@163.com','JNCMBLONBOPNOFTW',mail.write_email('18607706274@163.com',Findingmail,code),Findingmail)
 
-    return str(code5minute)   
+    redis_db.setex(str(Findingmail) + "_passwordforgetmail",300,code)
+    ##################################
+    # 获取所有键
+    keys = redis_db.keys()
+    print("--------------------加入时候数据库内数据")
+    # 遍历每个键，并输出键和对应的值
+    for key in keys:
+        value = redis_db.get(key)
+        print(key.decode(), "->", value.decode())
+    ##################################
+    mail.send_email('18607706274@163.com','JNCMBLONBOPNOFTW',mail.write_email('18607706274@163.com',Findingmail,code),Findingmail)
+    
+    return code 
 
 
 
